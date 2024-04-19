091ab0d3f5493cf10fb6548d9e22aa7a929a4ae7
Katina <direfox15@gmail.com>
2024-04-19 11:41:51 +0300

    add github job for running ui tests

--- tests/ui/games.test.js
+++ tests/ui/games.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check games page', async ({ page }) => {
-    await page.goto('http://localhost:8080/games');
+    await page.goto('http://localhost:8080/collection');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
